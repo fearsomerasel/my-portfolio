6c645a1a4baf8ce9446847bf65c1f76ae435ec25
fearsomerasel <97090384+fearsomerasel@users.noreply.github.com>
2022-07-02 18:16:00 +0600

    last commit

---
+++ new-blog.js
@@ -0,0 +1,11 @@
+const menuBar = document.getElementById("menu-bar");
+const menu = document.getElementById("menu");
+
+menuBar.addEventListener("click", ()=>{
+    if (menu.className==='hidden') {
+        menu.classList.remove('hidden');
+    }
+    else{
+        menu.classList.add('hidden');
+    }
+});
